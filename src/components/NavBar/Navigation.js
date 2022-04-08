fe9111190ffc74d6f099c597d54867058b74415a
Oksana Stets <oksanastets73@gmail.com>
2022-04-08 16:56:18 +0300

    Update Navigation.js

--- src/components/NavBar/Navigation.js
+++ src/components/NavBar/Navigation.js
@@ -8,38 +8,33 @@ import { authSelectors } from 'redux/auth';
 export default function Navigation() {
   const isLoggedIn = useSelector(authSelectors.getIsLoggedIn);
   return (
-  
-      <Nav>
-        <NavList>
-          <NavListItem>
-            <NavLink
-              to="/"
-              style={({ isActive }) => ({
-                color: isActive ? '#dc6300' : '#2a363b',
-                textDecoration: isActive ? 'underline' : 'none',
-              })}
-            >
-              Сторінка вітання
-            </NavLink>
-          </NavListItem>
-          <NavListItem>
-            <NavLink
-              to="/contacts"
-              style={({ isActive }) => ({
-                color: isActive ? '#dc6300' : '#2a363b',
-                textDecoration: isActive ? 'underline' : 'none',
-              })}
-            >
-              Сторінка користувача
-            </NavLink>
-          </NavListItem>
+    <Nav>
+      <NavList>
+        <NavListItem>
+          <NavLink
+            to="/"
+            style={({ isActive }) => ({
+              color: isActive ? '#dc6300' : '#2a363b',
+              textDecoration: isActive ? 'underline' : 'none',
+            })}
+          >
+            Сторінка вітання
+          </NavLink>
+        </NavListItem>
+        <NavListItem>
+          <NavLink
+            to="/contacts"
+            style={({ isActive }) => ({
+              color: isActive ? '#dc6300' : '#2a363b',
+              textDecoration: isActive ? 'underline' : 'none',
+            })}
+          >
+            Сторінка користувача
+          </NavLink>
+        </NavListItem>
       </NavList>
-      
-      { isLoggedIn ? <UserMenu /> : <AuthNav />}
-        
-        
-      </Nav>
-      
 
+      {isLoggedIn ? <UserMenu /> : <AuthNav />}
+    </Nav>
   );
 }
