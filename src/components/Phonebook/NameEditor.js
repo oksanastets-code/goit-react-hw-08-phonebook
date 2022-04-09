ba5777638399ff4e2c5c0646bbdb75ddd21bee53
Oksana Stets <oksanastets73@gmail.com>
2022-04-09 23:23:45 +0300

    fix bug

--- src/components/Phonebook/NameEditor.js
+++ src/components/Phonebook/NameEditor.js
@@ -1,12 +1,15 @@
 import { useState } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import toast from 'react-hot-toast';
 import { addContact } from '../../redux/PhoneBook/phone-book-operations';
 import PropTypes from 'prop-types';
 import { Form, Label, InputField } from './Form.styled';
 import { Button } from '../Button/Button.styled';
+import { getContacts } from 'redux/PhoneBook/phone-book-selectors';
 export default function NameEditor() {
   const [name, setName] = useState('');
   const [number, setNumber] = useState('');
+  const contacts = useSelector(getContacts);
   const dispatch = useDispatch();
 
   const handleForm = e => {
@@ -25,42 +28,51 @@ export default function NameEditor() {
 
   const handleSubmit = e => {
     e.preventDefault();
+
+    if (
+      contacts.find(
+        contact => contact.name.toLocaleLowerCase() === name.toLocaleLowerCase()
+      )
+    ) {
+      const notify = `${name} is already on list`;
+      toast.error(notify);
+      return;
+    }
     dispatch(addContact({ name, number }));
     setName('');
     setNumber('');
   };
 
   return (
-      <Form onSubmit={handleSubmit}>
-        <Label htmlFor="">
-          Name
-          <InputField
-            type="text"
-            name="name"
-            value={name}
-            onChange={handleForm}
-            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-            required
-            placeholder=""
-          />
-        </Label>
-        <Label htmlFor="">
-          Number
-          <InputField
-            type="tel"
-            name="number"
-            value={number}
-            onChange={handleForm}
-            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-            required
-            placeholder=""
-          />
-        </Label>
-        <Button type="submit">Add contact</Button>
-      </Form>
-
+    <Form onSubmit={handleSubmit}>
+      <Label htmlFor="">
+        Name
+        <InputField
+          type="text"
+          name="name"
+          value={name}
+          onChange={handleForm}
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          required
+          placeholder=""
+        />
+      </Label>
+      <Label htmlFor="">
+        Number
+        <InputField
+          type="tel"
+          name="number"
+          value={number}
+          onChange={handleForm}
+          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+          required
+          placeholder=""
+        />
+      </Label>
+      <Button type="submit">Add contact</Button>
+    </Form>
   );
 }
 
--- src/redux/PhoneBook/phone-book-reducer.js
+++ src/redux/PhoneBook/phone-book-reducer.js
@@ -18,11 +18,11 @@ const contacts = createReducer([],
   {
     [fetchContactsSuccess]: (_, { payload }) => payload,
     [addContactSuccess]: (state, { payload }) => { 
-         if (state.find(contact => contact.name === payload.name)) {
-        const notify = `${payload.name} is already on list`;
-        toast.error(notify);
-        return state;
-      }
+      //    if (state.find(contact => contact.name === payload.name)) {
+      //   const notify = `${payload.name} is already on list`;
+      //   toast.error(notify);
+      //   return state;
+      // }
       state = [...state, payload];
       toast.success(`Contact ${payload.name} added!`);
       return state;
